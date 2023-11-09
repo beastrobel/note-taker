e3ceec9fe534d8ee2f87e0107dc41dc4fb6640f2
beastrobel <beatrizstrobel@proton.me>
2023-11-08 22:29:02 -0500

    Added api route - needs debugging

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,13 @@ const fs = require('fs');
 const PORT = 3001;
 const app = express();
 
+// Import the feedback router
+const api = require('./public/assets/js/index');
+
+// Middleware for parsing JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //Middleware
 app.use(express.static('public'));
 
@@ -11,6 +18,14 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
+
+// Send all the requests that begin with /api to the index.js in the routes folder
+app.use('/api', api);
+
+//Route for note data
+
+//Listen for form submission
+
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
