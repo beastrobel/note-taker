7c8f5715866e3d2cbc36d00b46a3712b5f88f4b5
beastrobel <beatrizstrobel@proton.me>
2023-11-08 22:17:07 -0500

    Added middleware and static homepage route

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const path = require('path');
+const fs = require('fs');
 const PORT = 3001;
-
 const app = express();
+
+//Middleware
+app.use(express.static('public'));
+
+//Static homepage route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
